737f8f154def3a863baf6c2c29aa85434ac1c755
pal2711 <palgajera7@gmail.com>
2026-06-27 13:24:56 +0530

    Replace mysqlclient with PyMySQL

--- models/settings.py
+++ models/settings.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from django.contrib.messages import constants as messages
 from dotenv import load_dotenv
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
